f69dc07c723536a13bbc6e7cfb5a0b546fd5564e
mizuno <xak.dairou@gmail.com>
2020-10-28 23:26:10 +0900

    project追加

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int calc;
+            Console.WriteLine(" *  *  *  九九の表  *  *  *");
+
+            for (int virticalTable = 1; virticalTable < 10; virticalTable++)
+            {
+                Console.WriteLine(" ");
+                for (int horizontalTable = 1; horizontalTable < 10; horizontalTable++)
+                {
+                    calc = virticalTable * horizontalTable;
+                    Console.Write("{0,3}",calc);
+
+                }
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
